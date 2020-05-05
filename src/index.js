421398cfe881a51283560dc8b061cdc03396169c
Sanara Premaratne <sanarap9@gmail.com>
2020-05-06 01:53:33 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = (props) => {
+  return(
+    <div>
+    <p> 
+    Hello {props.name}, you are {props.age} years old
+    </p>
+  </div>
+)
+}
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+const nimi = 'Pekka'
+const ika = 10
+
+return (
+  <div>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={26 + 10} />
+    <Hello name={nimi} age={ika} />
+    <Footer/>
+  </div>
+)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
